2b1f3b6875a474acd6e4084c97de519179018b3c
zyc9012 <zyc9012@163.com>
2022-03-21 15:08:56 +0800

    chore: add empty sheet

--- stories/Workbook.stories.tsx
+++ stories/Workbook.stories.tsx
@@ -3,6 +3,7 @@ import { ComponentMeta, ComponentStory } from "@storybook/react";
 import { Workbook } from "@fortune-sheet/react/src";
 import cell from "./data/cell";
 import formula from "./data/formula";
+import empty from "./data/empty";
 
 export default {
   component: Workbook,
@@ -19,6 +20,9 @@ Basic.args = { data: [cell] };
 
 export const Formula = Template.bind({});
 Formula.args = { data: [formula] };
+
+export const Empty = Template.bind({});
+Empty.args = { data: [empty] };
 
 export const All = Template.bind({});
 All.args = { data: [cell, formula] };
---
+++ stories/data/empty.ts
@@ -0,0 +1,18 @@
+const data = {
+  name: "Empty",
+  status: 1,
+  celldata: [
+    {
+      r: 0,
+      c: 0,
+      v: null,
+    },
+    {
+      r: 30,
+      c: 30,
+      v: null,
+    },
+  ],
+};
+
+export default data;
