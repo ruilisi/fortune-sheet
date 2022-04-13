ea54f2502c316f1398f9f68e3c4aa7041342305d
zyc9012 <zyc9012@163.com>
2022-04-13 10:10:12 +0800

    feat: cell multi selection

--- packages/core/src/events/mouse.ts
+++ packages/core/src/events/mouse.ts
@@ -30,7 +30,7 @@ import {
   pasteHandlerOfPaintModel,
 } from "../modules/selection";
 import { Settings } from "../settings";
-import { GlobalCache, Selection } from "../types";
+import { GlobalCache } from "../types";
 import { getSheetIndex } from "../utils";
 
 let mouseWheelUniqueTimeout: ReturnType<typeof setTimeout>;
@@ -199,8 +199,6 @@ export function handleCellAreaMouseDown(
     [row_pre, row, row_index, row_index_ed] = margeset.row;
     [col_pre, col, col_index, col_index_ed] = margeset.column;
   }
-
-  let selection: Selection[];
 
   // //单元格单击之前
   // if (
@@ -933,161 +931,145 @@ export function handleCellAreaMouseDown(
   //   return;
   // }
 
-  // if (ctx.luckysheet_select_status) {
-  // if (e.shiftKey) {
-  //   // 按住shift点击，选择范围
-  //   const last = $.extend(
-  //     true,
-  //     {},
-  //     ctx.luckysheet_select_save[
-  //       ctx.luckysheet_select_save.length - 1
-  //     ]
-  //   ); // 选区最后一个
+  if (ctx.luckysheet_select_status) {
+    if (e.shiftKey) {
+      // 按住shift点击，选择范围
+      const last =
+        ctx.luckysheet_select_save?.[ctx.luckysheet_select_save.length - 1]; // 选区最后一个
+      if (
+        last &&
+        last.top != null &&
+        last.left != null &&
+        last.height != null &&
+        last.width != null &&
+        last.row_focus != null &&
+        last.column_focus != null
+      ) {
+        let top = 0;
+        let height = 0;
+        let rowseleted = [];
+        if (last.top > row_pre) {
+          top = row_pre;
+          height = last.top + last.height - row_pre;
+          if (last.row[1] > last.row_focus) {
+            last.row[1] = last.row_focus;
+          }
+          rowseleted = [row_index, last.row[1]];
+        } else if (last.top === row_pre) {
+          top = row_pre;
+          height = last.top + last.height - row_pre;
+          rowseleted = [row_index, last.row[0]];
+        } else {
+          top = last.top;
+          height = row - last.top - 1;
+          if (last.row[0] < last.row_focus) {
+            last.row[0] = last.row_focus;
+          }
+          rowseleted = [last.row[0], row_index];
+        }
+        let left = 0;
+        let width = 0;
+        let columnseleted = [];
+        if (last.left > col_pre) {
+          left = col_pre;
+          width = last.left + last.width - col_pre;
+          if (last.column[1] > last.column_focus) {
+            last.column[1] = last.column_focus;
+          }
+          columnseleted = [col_index, last.column[1]];
+        } else if (last.left === col_pre) {
+          left = col_pre;
+          width = last.left + last.width - col_pre;
+          columnseleted = [col_index, last.column[0]];
+        } else {
+          left = last.left;
+          width = col - last.left - 1;
+          if (last.column[0] < last.column_focus) {
+            last.column[0] = last.column_focus;
+          }
+          columnseleted = [last.column[0], col_index];
+        }
+        const changeparam = mergeMoveMain(
+          ctx,
+          columnseleted,
+          rowseleted,
+          last,
+          top,
+          height,
+          left,
+          width
+        );
+        if (changeparam != null) {
+          // @ts-ignore
+          [columnseleted, rowseleted, top, height, left, width] = changeparam;
+        }
+        last.row = rowseleted;
+        last.column = columnseleted;
+        last.left_move = left;
+        last.width_move = width;
+        last.top_move = top;
+        last.height_move = height;
+        ctx.luckysheet_select_save![ctx.luckysheet_select_save!.length - 1] =
+          last;
+        // // 交替颜色选择范围
+        // if ($("#luckysheet-alternateformat-rangeDialog").is(":visible")) {
+        //   $("#luckysheet-alternateformat-rangeDialog input").val(
+        //     getRangetxt(ctx.currentSheetIndex, ctx.luckysheet_select_save)
+        //   );
+        // }
+        // if (pivotTable.luckysheet_pivotTable_select_state) {
+        //   $("#luckysheet-pivotTable-range-selection-input").val(
+        //     `${
+        //       ctx.luckysheetfile[getSheetIndex(ctx.currentSheetIndex)]
+        //         .name
+        //     }!${chatatABC(ctx.luckysheet_select_save[0].column[0])}${
+        //       ctx.luckysheet_select_save[0].row[0] + 1
+        //     }:${chatatABC(ctx.luckysheet_select_save[0].column[1])}${
+        //       ctx.luckysheet_select_save[0].row[1] + 1
+        //     }`
+        //   );
+      }
+    } else if (e.ctrlKey || e.metaKey) {
+      // 选区添加
+      ctx.luckysheet_select_save?.push({
+        left: col_pre,
+        width: col - col_pre - 1,
+        top: row_pre,
+        height: row - row_pre - 1,
+        left_move: col_pre,
+        width_move: col - col_pre - 1,
+        top_move: row_pre,
+        height_move: row - row_pre - 1,
+        row: [row_index, row_index_ed],
+        column: [col_index, col_index_ed],
+        row_focus: row_index,
+        column_focus: col_index,
+      });
+    } else {
+      // eslint-disable-next-line prefer-const
+      ctx.luckysheet_select_save = [
+        {
+          left: col_pre,
+          width: col - col_pre - 1,
+          top: row_pre,
+          height: row - row_pre - 1,
+          left_move: col_pre,
+          width_move: col - col_pre - 1,
+          top_move: row_pre,
+          height_move: row - row_pre - 1,
+          row: [row_index, row_index_ed],
+          column: [col_index, col_index_ed],
+          row_focus: row_index,
+          column_focus: col_index,
+        },
+      ];
 
-  //   let top = 0;
-  //   let height = 0;
-  //   let rowseleted = [];
-  //   if (last.top > row_pre) {
-  //     top = row_pre;
-  //     height = last.top + last.height - row_pre;
-
-  //     if (last.row[1] > last.row_focus) {
-  //       last.row[1] = last.row_focus;
-  //     }
-
-  //     rowseleted = [row_index, last.row[1]];
-  //   } else if (last.top == row_pre) {
-  //     top = row_pre;
-  //     height = last.top + last.height - row_pre;
-  //     rowseleted = [row_index, last.row[0]];
-  //   } else {
-  //     top = last.top;
-  //     height = row - last.top - 1;
-
-  //     if (last.row[0] < last.row_focus) {
-  //       last.row[0] = last.row_focus;
-  //     }
-
-  //     rowseleted = [last.row[0], row_index];
-  //   }
-
-  //   let left = 0;
-  //   let width = 0;
-  //   let columnseleted = [];
-  //   if (last.left > col_pre) {
-  //     left = col_pre;
-  //     width = last.left + last.width - col_pre;
-
-  //     if (last.column[1] > last.column_focus) {
-  //       last.column[1] = last.column_focus;
-  //     }
-
-  //     columnseleted = [col_index, last.column[1]];
-  //   } else if (last.left == col_pre) {
-  //     left = col_pre;
-  //     width = last.left + last.width - col_pre;
-  //     columnseleted = [col_index, last.column[0]];
-  //   } else {
-  //     left = last.left;
-  //     width = col - last.left - 1;
-
-  //     if (last.column[0] < last.column_focus) {
-  //       last.column[0] = last.column_focus;
-  //     }
-
-  //     columnseleted = [last.column[0], col_index];
-  //   }
-
-  //   const changeparam = menuButton.mergeMoveMain(
-  //     columnseleted,
-  //     rowseleted,
-  //     last,
-  //     top,
-  //     height,
-  //     left,
-  //     width
-  //   );
-  //   if (changeparam != null) {
-  //     columnseleted = changeparam[0];
-  //     rowseleted = changeparam[1];
-  //     top = changeparam[2];
-  //     height = changeparam[3];
-  //     left = changeparam[4];
-  //     width = changeparam[5];
-  //   }
-
-  //   last.row = rowseleted;
-  //   last.column = columnseleted;
-
-  //   last.left_move = left;
-  //   last.width_move = width;
-  //   last.top_move = top;
-  //   last.height_move = height;
-
-  //   ctx.luckysheet_select_save[
-  //     ctx.luckysheet_select_save.length - 1
-  //   ] = last;
-
-  //   // 交替颜色选择范围
-  //   if ($("#luckysheet-alternateformat-rangeDialog").is(":visible")) {
-  //     $("#luckysheet-alternateformat-rangeDialog input").val(
-  //       getRangetxt(ctx.currentSheetIndex, ctx.luckysheet_select_save)
-  //     );
-  //   }
-
-  //   if (pivotTable.luckysheet_pivotTable_select_state) {
-  //     $("#luckysheet-pivotTable-range-selection-input").val(
-  //       `${
-  //         ctx.luckysheetfile[getSheetIndex(ctx.currentSheetIndex)]
-  //           .name
-  //       }!${chatatABC(ctx.luckysheet_select_save[0].column[0])}${
-  //         ctx.luckysheet_select_save[0].row[0] + 1
-  //       }:${chatatABC(ctx.luckysheet_select_save[0].column[1])}${
-  //         ctx.luckysheet_select_save[0].row[1] + 1
-  //       }`
-  //     );
-  //   }
-  // } else if (e.ctrlKey) {
-  //   // 选区添加
-  //   ctx.luckysheet_select_save.push({
-  //     left: col_pre,
-  //     width: col - col_pre - 1,
-  //     top: row_pre,
-  //     height: row - row_pre - 1,
-  //     left_move: col_pre,
-  //     width_move: col - col_pre - 1,
-  //     top_move: row_pre,
-  //     height_move: row - row_pre - 1,
-  //     row: [row_index, row_index_ed],
-  //     column: [col_index, col_index_ed],
-  //     row_focus: row_index,
-  //     column_focus: col_index,
-  //   });
-  // } else {
-
-  // eslint-disable-next-line prefer-const
-  selection = [
-    {
-      left: col_pre,
-      width: col - col_pre - 1,
-      top: row_pre,
-      height: row - row_pre - 1,
-      left_move: col_pre,
-      width_move: col - col_pre - 1,
-      top_move: row_pre,
-      height_move: row - row_pre - 1,
-      row: [row_index, row_index_ed],
-      column: [col_index, col_index_ed],
-      row_focus: row_index,
-      column_focus: col_index,
-    },
-  ];
-
-  // 单元格格式icon对应
-  //   menuButton.menuButtonFocus(ctx.flowdata, row_index, col_index);
-  //   // 函数公式显示栏
-  //   formula.fucntionboxshow(row_index, col_index);
-  // }
+      // 单元格格式icon对应
+      //   menuButton.menuButtonFocus(ctx.flowdata, row_index, col_index);
+      //   // 函数公式显示栏
+      //   formula.fucntionboxshow(row_index, col_index);
+    }
+  }
 
   // selectHightlightShow();
 
@@ -1145,7 +1127,10 @@ export function handleCellAreaMouseDown(
   //   sheetFile,
   //   luckysheetTableContent
   // );
-  ctx.luckysheet_select_save = normalizeSelection(ctx, selection);
+  ctx.luckysheet_select_save = normalizeSelection(
+    ctx,
+    ctx.luckysheet_select_save
+  );
 }
 
 export function handleCellAreaDoubleClick(
