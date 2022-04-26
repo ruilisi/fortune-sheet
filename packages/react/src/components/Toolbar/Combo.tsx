222d121b49446d8e5f59da471b17b298703f6968
zyc9012 <zyc9012@163.com>
2022-04-26 14:16:23 +0800

    chore: toolbar combo popup positioning

--- packages/react/src/components/Toolbar/Combo.tsx
+++ packages/react/src/components/Toolbar/Combo.tsx
@@ -1,4 +1,4 @@
-import React, { CSSProperties, useRef, useState } from "react";
+import React, { CSSProperties, useLayoutEffect, useRef, useState } from "react";
 import { useOutsideClick } from "../../hooks/useOutsideClick";
 import SVGIcon from "../SVGIcon";
 
@@ -21,15 +21,34 @@ const Combo: React.FC<Props> = ({
 }) => {
   const style: CSSProperties = { userSelect: "none" };
   const [open, setOpen] = useState(false);
+  const [popupPosition, setPopupPosition] = useState({ left: 0 });
   const popupRef = useRef<HTMLDivElement>(null);
+  const buttonRef = useRef<HTMLDivElement>(null);
 
   useOutsideClick(popupRef, () => {
     setOpen(false);
   });
 
+  useLayoutEffect(() => {
+    // re-position the popup menu if it overflows the window
+    if (!popupRef.current) {
+      return;
+    }
+    if (!open) {
+      setPopupPosition({ left: 0 });
+    }
+    const winW = window.innerWidth;
+    const rect = popupRef.current.getBoundingClientRect();
+    const menuW = rect.width;
+    const { left } = rect;
+    if (left + menuW > winW) {
+      setPopupPosition({ left: -rect.width + buttonRef.current!.clientWidth });
+    }
+  }, [open]);
+
   return (
     <div className="fortune-toobar-combo-container fortune-toolbar-item">
-      <div className="fortune-toolbar-combo">
+      <div ref={buttonRef} className="fortune-toolbar-combo">
         <div
           className="fortune-toolbar-combo-button"
           onClick={(e) => {
@@ -58,7 +77,11 @@ const Combo: React.FC<Props> = ({
         {tooltip && <div className="fortune-tooltip">{tooltip}</div>}
       </div>
       {open && (
-        <div ref={popupRef} className="fortune-toolbar-combo-popup">
+        <div
+          ref={popupRef}
+          className="fortune-toolbar-combo-popup"
+          style={popupPosition}
+        >
           {children?.(setOpen)}
         </div>
       )}
