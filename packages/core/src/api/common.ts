a322e5c6328a972c1a44196842999fce387ad732
Arthur <mysteryyoongtu@163.com>
2022-11-18 18:33:03 +0800

    fix: update celldata when calling getSheet

--- packages/core/src/api/common.ts
+++ packages/core/src/api/common.ts
@@ -1,8 +1,25 @@
 import { Context } from "../context";
+import { CellMatrix, CellWithRowAndCol } from "../types";
 import { getSheetIndex } from "../utils";
 import { SHEET_NOT_FOUND } from "./errors";
 
 export type CommonOptions = { index?: number; id?: string };
+
+const dataToCelldata = (data: CellMatrix | undefined) => {
+  const celldata: CellWithRowAndCol[] = [];
+  if (data == null) {
+    return celldata;
+  }
+  for (let r = 0; r < data.length; r += 1) {
+    for (let c = 0; c < data[r].length; c += 1) {
+      const v = data[r][c];
+      if (v != null) {
+        celldata.push({ r, c, v });
+      }
+    }
+  }
+  return celldata;
+};
 
 export function getSheet(ctx: Context, options: CommonOptions = {}) {
   const { index = getSheetIndex(ctx, options.id || ctx.currentSheetId) } =
@@ -18,5 +35,5 @@ export function getSheet(ctx: Context, options: CommonOptions = {}) {
     throw SHEET_NOT_FOUND;
   }
 
-  return sheet;
+  return { ...sheet, celldata: dataToCelldata(sheet.data) };
 }
