a17da1beff7c6e9dbf4baf0b56b4c28fc9d0dee2
lvjing <542918096@qq.com>
2023-01-16 15:05:44 +0800

    feat: Displays the information in the selection

--- packages/react/src/components/SheetTab/index.tsx
+++ packages/react/src/components/SheetTab/index.tsx
@@ -6,7 +6,9 @@ import React, {
   useRef,
   useState,
 } from "react";
-import { updateCell, addSheet } from "@fortune-sheet/core";
+import { updateCell, addSheet, locale, getFlowdata } from "@fortune-sheet/core";
+// @ts-ignore
+import SSF from "@fortune-sheet/core/src/modules/ssf.js";
 import WorkbookContext from "../../context";
 import SVGIcon from "../SVGIcon";
 import "./index.css";
@@ -17,10 +19,18 @@ const SheetTab: React.FC = () => {
   const tabContainerRef = useRef<HTMLDivElement>(null);
   const leftScrollRef = useRef<HTMLDivElement>(null);
   const rightScrollRef = useRef<HTMLDivElement>(null);
+  const { formula } = locale(context);
   const [sheetScrollAni, setSheetScrollAni] = useState<any>(null);
   const [sheetScrollStep] = useState<number>(150);
   const [isShowScrollBtn, setIsShowScrollBtn] = useState<boolean>(false);
   const [isShowBoundary, setIsShowBoundary] = useState<boolean>(true);
+  const [numberCount, setNumberCount] = useState<number>(0);
+  const [showCalInfo, setShowCalInfo] = useState<boolean>(false);
+  const [countInfo, setCountInfo] = useState<string | undefined>(undefined);
+  const [sumInfo, setSumInfo] = useState<string | undefined>(undefined);
+  const [averageInfo, setAverageInfo] = useState<string | undefined>(undefined);
+  const [maxInfo, setMaxInfo] = useState<string | undefined>(undefined);
+  const [minInfo, setMinInfo] = useState<string | undefined>(undefined);
 
   const scrollToLeft = useCallback(
     (moveType: string) => {
@@ -71,6 +81,61 @@ const SheetTab: React.FC = () => {
     const tabCurrent = tabContainerRef.current;
     setIsShowScrollBtn(tabCurrent!.scrollWidth > tabCurrent!.clientWidth);
   }, []);
+
+  // 计算选区的信息
+  useEffect(() => {
+    const selection = context.luckysheet_select_save;
+    if (selection) {
+      const data = getFlowdata(context, context.currentSheetId);
+      if (data == null) return;
+      const row = selection[0].row ?? [];
+      const column = selection[0].column ?? [];
+      if (row[0] !== row[1] || column[0] !== column[1]) {
+        let numberC = 0;
+        let count = 0;
+        let sum = 0;
+        let max = Number.MIN_VALUE;
+        let min = Number.MAX_VALUE;
+        for (let r = row[0]; r <= row[1]; r += 1) {
+          for (let c = column[0]; c <= column[1]; c += 1) {
+            if (r >= data.length || c >= data[0].length) break;
+            const value = data![r][c]?.m as string;
+            // 判断是不是数字
+            if (parseFloat(value).toString() !== "NaN") {
+              const valueNumber = parseFloat(value);
+              count += 1;
+              sum += valueNumber;
+              max = Math.max(valueNumber, max);
+              min = Math.min(valueNumber, min);
+              numberC += 1;
+            } else if (value != null) {
+              count += 1;
+            }
+          }
+        }
+        const average = SSF.format("w0.00", sum / numberC);
+        sum = SSF.format("w0.00", sum);
+        max = SSF.format("w0.00", max === Number.MIN_VALUE ? 0 : max);
+        min = SSF.format("w0.00", min === Number.MAX_VALUE ? 0 : min);
+        setCountInfo(count.toString());
+        setSumInfo(sum.toString());
+        setAverageInfo(average.toString());
+        setMaxInfo(max.toString());
+        setMinInfo(min.toString());
+        setShowCalInfo(true);
+        setNumberCount(numberC);
+      } else {
+        setCountInfo(undefined);
+        setSumInfo(undefined);
+        setAverageInfo(undefined);
+        setMaxInfo(undefined);
+        setMinInfo(undefined);
+        setShowCalInfo(false);
+        setNumberCount(0);
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [context.luckysheet_select_save]);
 
   return (
     <div
@@ -182,6 +247,35 @@ const SheetTab: React.FC = () => {
             }}
           >
             <SVGIcon name="arrow-doubleright" width={12} height={12} />
+          </div>
+        )}
+        {showCalInfo && (
+          <div className="luckysheet-sheet-selection-calInfo">
+            {!!countInfo && (
+              <div style={{ width: "60px" }}>
+                {formula.count}: {countInfo}
+              </div>
+            )}
+            {!!numberCount && !!sumInfo && (
+              <div>
+                {formula.sum}: {sumInfo}
+              </div>
+            )}
+            {!!numberCount && !!averageInfo && (
+              <div>
+                {formula.average}: {averageInfo}
+              </div>
+            )}
+            {!!numberCount && !!maxInfo && (
+              <div>
+                {formula.max}: {maxInfo}
+              </div>
+            )}
+            {!!numberCount && !!minInfo && (
+              <div>
+                {formula.min}: {minInfo}
+              </div>
+            )}
           </div>
         )}
       </div>
