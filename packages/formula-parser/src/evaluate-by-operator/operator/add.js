5533cb941996adb6899b972b4f524c6423de449b
Arthur <mysteryyoongtu@163.com>
2023-03-29 01:13:16 +0800

    fix: add undefined cell as zero

--- packages/formula-parser/src/evaluate-by-operator/operator/add.js
+++ packages/formula-parser/src/evaluate-by-operator/operator/add.js
@@ -4,7 +4,7 @@ import {ERROR_VALUE} from './../../error';
 export const SYMBOL = '+';
 
 export default function func(first, ...rest) {
-  const result = rest.reduce((acc, value) => acc + toNumber(value), toNumber(first));
+  const result = rest.reduce((acc, value) => acc + toNumber(value || 0), toNumber(first || 0));
 
   if (isNaN(result)) {
     throw Error(ERROR_VALUE);
