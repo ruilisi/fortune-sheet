71b8164bec608203ab64bf3b36c73e2225a2ae51
Shashank Agarwal <53386582+Corbe30@users.noreply.github.com>
2024-11-11 09:23:10 +0000

    fixed: semicolon

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -253,7 +253,7 @@ export type Freezen = {
 };
 
 export type GlobalCache = {
-  verticalScrollLock?: boolean,
+  verticalScrollLock?: boolean;
   overwriteCell?: boolean;
   ignoreWriteCell?: boolean;
   doNotFocus?: boolean;
